032c7670107d804de5c52b04fa2f8e0dc5e8a388
James <adolfssonjames@gmail.com>
2022-06-02 23:40:15 +0200

    Mycket uppdateringar och ändringar

--- App.js
+++ App.js
@@ -1,42 +1,37 @@
 import React, { useState, useEffect } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, KeyboardAvoidingView, TextInput, TouchableOpacity, Keyboard,ScrollView, FlatList } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput, TouchableOpacity, FlatList, Alert } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-import TodoTask from './components/TodoTask';
+
 const Stack = createNativeStackNavigator();
 
 // Första Sidan
-function Sysslor ({navigation}) {
- 
-  const [task, setTask] = useState();
-  const [taskItems, setTaskItems] = useState([]);
-  
+function PunList ({navigation}) {
+
   const [search, setSearch] = useState('');
   const [filteredDataSource, setFilteredDataSource] = useState([]);
   const [mainDataSource, setMainDataSource] = useState([]);
 
-  
   useEffect(() => {
     fetch('https://jsonplaceholder.typicode.com/todos')
       .then((response) => response.json())
       .then((responseJson) => {
         setFilteredDataSource(responseJson);
         setMainDataSource(responseJson);
+        console.log(responseJson);
       })
       .catch((error) => {
         console.error(error);
       });
   }, []);
 
-
   const searchFilterFunction = (text) => {
-    // kollar om filter fältet inte är tom
+    
     if (text) {
       // om den inte är tom
       // Filtrera mainDataSource och uppdatera filteredDataSource
-      
       const newData = mainDataSource.filter(
         function (item) {
           const itemData = item.title
@@ -47,33 +42,35 @@ function Sysslor ({navigation}) {
       });
       setFilteredDataSource(newData);
       setSearch(text);
-    } else {
-      
+    }  
+    else {
       // Updaterar FilteredDataSource med mainDataSource
       setFilteredDataSource(mainDataSource);
       setSearch(text);
     }
   };
 
-
-  // FlatList Item
-  const ItemView = ({item}) => {
+  // FlatList Item klick navigering funktion
+  const ItemView = ({item,}) => {
+    
     return (
+      <View  > 
       <Text
-        style={styles.itemStyle}
-        onPress={() => getItem(item)}>
+        style={styles.punListStyle}
+        onPress={() => navigation.navigate('Learn', {data : getItem(item) })}>
         {item.id}
-        {'.'}
+        {".  "}  
         {item.title.toUpperCase()}
       </Text>
+      </View>   
     );
   };
-
 
   // delar upp listans items 
   const ItemSeparatorView = () => {
     return (
-      <View
+      <View 
+      
         style={{
           height: 1.5,
           width: '100%',
@@ -83,123 +80,83 @@ function Sysslor ({navigation}) {
     );
   };
 
-  //klicka på ett item funktion i filtrera listan
+  //klicka på ett item  
   const getItem = (item) => {
-  alert('Id : ' + item.id + ' Title : ' + item.title);
+    return (
+      <View >
+        <Text style={styles.chosenPunStyle}>
+        { ' Chosen PUN | ' + item.id + '. ' + item.title}
+        </Text>
+      </View>
+  
+    );
+  
   };
 
-  
 
-  //lägg till task
-  const handleNewTask = () => {
-    if(task.length > 2 ) {
-    Keyboard.dismiss(); //tar bort skrivbordet på mobil automatiskt vid task tillägg
-    setTaskItems ([...taskItems, task]) //lägger till det som redan finns i taskitem arrayen samt lägger till den nya tasken
-    setTask(null); // tömmer textfältet vid task tillägg.
-      
-    } else {
-      alert('3 bokstäver minimum krävs för att sysslan ska läggas till')
-    }
-  }
-// Ta bort task
-  const deleteTask = (index) => {
-    let copyItems = [...taskItems]; //skapar en ny array från taskitems och storeas i copyItems veriabeln
-    copyItems.splice(index, 1); // tar bort den valda itemet och sparar / storear resten av arrayen
-    setTaskItems(copyItems);
-  }
-
-  
-  
   return ( 
- 
-      <View style={styles.container1}>
-          <StatusBar style="auto" backgroundColor="#f7da59"  />
-          <TextInput
-          style={styles.textInputStyle}
-          onChangeText={(text) => searchFilterFunction(text)}
-          value={search}
-          placeholder="Filtrera här.."
-        />
-        <FlatList
-          data={filteredDataSource}
-          keyExtractor={(item, index) => index.toString()}
-          ItemSeparatorComponent={ItemSeparatorView}
-          renderItem={ItemView}
-        />
-        
-          <View style={styles.todoTaskContainer}>
+    <View style={styles.container1}>
+        <StatusBar style="auto" backgroundColor="#f7da59"  />
+        <View style={styles.todoTaskContainer}>
             <Text style={styles.title}>
-              Sysslor att göra
+              Roliga PUNS att lära sig
             </Text>
+            <TextInput
+             style={styles.textInputStyle}
+             onChangeText={(text) => searchFilterFunction(text) }
+             value={search}
+             placeholder="Search Pun"
+             />
+            <FlatList
+              style={styles.punListStyle}
+              data={filteredDataSource}
+              keyExtractor={(item, index) => index.toString()}
+              ItemSeparatorComponent={ItemSeparatorView}
+              renderItem={ItemView}
+             />
+        </View>
 
-            <ScrollView style={styles.taskItems}>
-
-              {
-                taskItems.map((item, index) => {  //itirerar genom arrayen och visar items i listan.
-                return (
-                  
-                  <TouchableOpacity 
-                     onPress={() => navigation.navigate('Påbörjad', {data : <TodoTask  style={styles.todoItem} text={item} /> })}>
-                     <TouchableOpacity style={(styles.raderaKnapp)} title="X" key={index} onPress={() => deleteTask(index)}>
-                       <Text>Ta Bort</Text>
-                     </TouchableOpacity>
-
-                     <TodoTask  style={styles.todoItem} text={item} /> 
-                  
-                   </TouchableOpacity>
-                  )
-                })
-              }
-                  
-
-            </ScrollView>
-          </View>
-
-          <KeyboardAvoidingView behavior={Platform.OS === "ios" ? "padding" : "height"} //om det är en iphone, ge padding annars om android ge height från keyboard.       //rad 33: varje gång texten ändras, fångar vi texten och settar staten task till att "bli" den texten som skrivits in.
-          style={styles.typeTaskSection}>
-                                                                                                            
-            <TextInput  
-            maxLength={35} 
-            style={styles.taskInput} 
-            placeholder={'Skriv en sysslaa'} 
-            value={task} 
-            onChangeText={text => setTask(text)}  
-            />     
-            
-            
-            <TouchableOpacity onPress={() =>  handleNewTask() }>
-            <View style={styles.addBtn}>
-              <Text style={styles.addTask}>Lägg till</Text>
-            </View>
-            </TouchableOpacity>
-          </KeyboardAvoidingView>
-           
       
-      </View>
-    )
-}
+    </View>
+    );
+};
 // Andra Sidan
-function Påbörjad({navigation, route}) {
+function Learn ({navigation, route}) {
+
   return (
-    <View style={styles.taskItems}>
-      
-      <TouchableOpacity
-       onPress={() => navigation.navigate('Färdig', {data : route.params.data})}>{route.params.data}
-      </TouchableOpacity>
+    <View style={styles.punListStyle}>
       <StatusBar style="auto" backgroundColor="#f7da59" />
+      <TouchableOpacity
+         onPress={() => navigation.navigate('Learned', {data : route.params.data})}>{route.params.data}
+      </TouchableOpacity>
+    
     </View>
   )
 }
 // Tredje Sidan
-function Färdig({navigation, route}) {
+function Learned({navigation, route}) {
 
   return (
     <View style={styles.container}>
-      <TouchableOpacity >
-
-      {route.params.data}
-      </TouchableOpacity>
       <StatusBar style="auto" backgroundColor="#f7da59" />
+      <TouchableOpacity 
+       onPress={() => navigation.navigate('Mastered', {data : route.params.data})}>{route.params.data}
+      </TouchableOpacity>
+      
+    </View>
+  )
+}
+//Fjärde sidan
+function Mastered({navigation, route}) {
+  
+  const onRemove = id => e => {
+    setMainDataSource(title.filter(title => title.id !== id));
+  };
+  return (
+    <View style={styles.container}>
+      <StatusBar style="auto" backgroundColor="#f7da59" />
+      {route.params.data}
+      <Button color="#ff2121" title="delete" onPress={() => navigation.navigate('PunList',  onRemove())} />
     </View>
   )
 }
@@ -208,9 +165,10 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Att göra" component={Sysslor} />
-        <Stack.Screen name="Påbörjad" component={Påbörjad} />
-        <Stack.Screen name="Färdig" component={Färdig} />
+        <Stack.Screen name="PunList" component={PunList} />
+        <Stack.Screen name="Learn" component={Learn} />
+        <Stack.Screen name="Learned" component={Learned} />
+        <Stack.Screen name="Mastered" component={Mastered} />
       </Stack.Navigator>
     </NavigationContainer>
   )  
@@ -219,10 +177,6 @@ export default function App() {
               
       
           
-        
-
-
-
 /* CSS */
 const styles = StyleSheet.create({
   container: {
@@ -231,76 +185,36 @@ const styles = StyleSheet.create({
    
   },
   container1: {
-    flex: 1,
-  },
-  todoTaskContainer: {
-    maxHeight:450,
-    paddingHorizontal:15,
-    paddingTop:0,
+    paddingLeft:40,
+    paddingRight:40,
   },
   title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-    paddingHorizontal:0,
+  fontSize:25, 
+  fontWeight:"bold",
+  alignSelf:'center',
   },
 
-
-  addBtn: {
-    width:60,
-    height: 60,
-    backgroundColor: '#9aff91',
-    borderRadius:15,
-    borderColor:'#19fa05',
-    borderWidth:1.2,
-    justifyContent: 'center',
-    alignItems: 'center'
-  },
-
-  taskInput: {
-    borderRadius:10,
-    borderColor:'#19fa05',
-    borderWidth:1.2,
-    paddingHorizontal:30,
-    paddingVertical:10,
-    width:200,
-    backgroundColor: '#9aff91',
-    fontSize: 17,
-  },
-
-  typeTaskSection: {
-    position: 'absolute',
-    bottom: 30,
-    width:'100%',
-    flexDirection: 'row',
-    justifyContent: 'space-evenly',
-    alignItems:'center',
-  },
-
-  raderaKnapp:{
-    width:50,
-    height:20,
-    backgroundColor: '#f01114',
-    borderRadius:5,
-    alignItems: 'center'
-    
-  }, 
-  filterTask:{
-    borderRadius:10,
-    borderColor:'#19fa05',
-    borderWidth:1.2,
-    paddingHorizontal:20,
-    paddingVertical:10,
-    width:150,
-    backgroundColor: '#fff',
-    fontSize: 17,
-    flex:3,
-    marginVertical: 10,
-  },
   textInputStyle: {
+   borderRadius: 50,
+  paddingLeft:22,
+    fontSize:30,
+    backgroundColor:"#a5faa5",
+    color:"#000300",
+    
+    
+    
+    
+  },
+
+  punListStyle: {
     fontSize:20,
-  }
+    paddingTop:5,
+  },
 
-
+  chosenPunStyle: {
+    color:"#45ad18",
+    fontSize:22,
+  },
 
 
 });
